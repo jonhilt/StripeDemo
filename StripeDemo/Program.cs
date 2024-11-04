26598cd3cfdc7813e2cab6be2042f5253a27c860
Jon Hilton <hilton.jon@gmail.com>
2024-11-04 10:42:57 +0000

    Initial Commit

---
+++ StripeDemo/Program.cs
@@ -0,0 +1,28 @@
+using Stripe;
+using StripeDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+StripeConfiguration.ApiKey = app.Configuration.GetValue<string>("StripeAPIKey");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
